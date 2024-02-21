0112a5fa996811864b4a155dd1cbf98bf7b02907
Gaby Noriega N <125925256+noriegaby@users.noreply.github.com>
2024-02-21 15:02:19 -0600

    Update index.js

--- final_project/index.js
+++ final_project/index.js
@@ -1,30 +1,89 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
-let books = require("./booksdb.js");
-const regd_users = express.Router();
+const session = require('express-session')
+const customer_routes = require('./router/auth_users.js').authenticated;
+const genl_routes = require('./router/general.js').general;
+const app = express();
+app.use(express.json());
 
-let users = [];
-
-const isValid = (username)=>{ //returns boolean
-  //Write your code hereif 
+let users = []
+const doesExist = (username)=>{
+  let userswithsamename = users.filter((user)=>{
+    return user.username === username
+  });
+  if(userswithsamename.length > 0){
+    return true;
+  } else {
+    return false;
+  }
+}
+const authenticatedUser = (username,password)=>{
+  let validusers = users.filter((user)=>{
+    return (user.username === username && user.password === password)
+  });
+  if(validusers.length > 0){
+    return true;
+  } else {
+    return false;
+  }
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
-}
 
-//only registered users can login
-regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+app.use(session({secret:"fingerpint"}))
 
-// Add a book review
-regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+app.use("/customer/auth/*", function auth(req,res,next){
+    if(req.session.authorization) {
+        token = req.session.authorization['accessToken'];
+        jwt.verify(token, "access",(err,user)=>{
+            if(!err){
+                req.user = user;
+                next();
+            }
+            else{
+                return res.status(403).json({message: "User not authenticated"})
+            }
+         });
+     } else {
+         return res.status(403).json({message: "User not logged in"})
+     }
+ });
+ app.post("/login", (req,res) => {
+   const username = req.body.username;
+   const password = req.body.password;
+   if (!username || !password) {
+       return res.status(404).json({message: "Error logging in"});
+   }
+   if (authenticatedUser(username,password)) {
+     let accessToken = jwt.sign({
+       data: password
+     }, 'access', { expiresIn: 60 * 60 });
+     req.session.authorization = {
+       accessToken,username
+   }
+   return res.status(200).send("User successfully logged in");
+   } else {
+     return res.status(208).json({message: "Invalid Login. Check username and password"});
+   }
+ });
+ app.post("/register", (req,res) => {
+   const username = req.body.username;
+   const password = req.body.password;
+   if (username && password) {
+     if (!doesExist(username)) { 
+       users.push({"username":username,"password":password});
+       return res.status(200).json({message: "User successfully registred. Now you can login"});
+     } else {
+       return res.status(404).json({message: "User already exists!"});    
+     }
+   } 
+   return res.status(404).json({message: "Unable to register user."});
+ });
+ 
+const PORT =5500;
 
-module.exports.authenticated = regd_users;
-module.exports.isValid = isValid;
-module.exports.users = users;
+app.use("/customer", customer_routes);
+
+app.use("/", genl_routes);
+
+app.listen(PORT,()=>console.log("Server is running in port 5500"));
+
