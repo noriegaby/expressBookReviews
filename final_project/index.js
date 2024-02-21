50e794a581990c413b67d1e4dd7669b6c7cca7e5
Gaby Noriega N <125925256+noriegaby@users.noreply.github.com>
2024-02-21 14:59:03 -0600

    Update index.js

--- final_project/index.js
+++ final_project/index.js
@@ -1,22 +1,30 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
-const session = require('express-session')
-const customer_routes = require('./router/auth_users.js').authenticated;
-const genl_routes = require('./router/general.js').general;
+let books = require("./booksdb.js");
+const regd_users = express.Router();
 
-const app = express();
+let users = [];
 
-app.use(express.json());
+const isValid = (username)=>{ //returns boolean
+  //Write your code hereif 
+}
 
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+const authenticatedUser = (username,password)=>{ //returns boolean
+//write code to check if username and password match the one we have in records.
+}
 
-app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+//only registered users can login
+regd_users.post("/login", (req,res) => {
+  //Write your code here
+  return res.status(300).json({message: "Yet to be implemented"});
 });
- 
-const PORT =5000;
 
-app.use("/customer", customer_routes);
-app.use("/", genl_routes);
+// Add a book review
+regd_users.put("/auth/review/:isbn", (req, res) => {
+  //Write your code here
+  return res.status(300).json({message: "Yet to be implemented"});
+});
 
-app.listen(PORT,()=>console.log("Server is running"));
+module.exports.authenticated = regd_users;
+module.exports.isValid = isValid;
+module.exports.users = users;
